762297cdabdb419130ce1fa1fa77b01834880473
João Castro <up202206575@up.pt>
2025-11-28 21:17:23 +0000

    Update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/joaop/uni/DA/untitled5 - Cópia
+# Install script for directory: C:/Users/joaop/uni/DA/DA2324_PRJ2_G10_5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled5")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/joaop/uni/DA/untitled5 - Cópia/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/joaop/uni/DA/DA2324_PRJ2_G10_5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
-#include "Proj/WaterSupply.h" // Include the definition of the WaterSupply class.
-#include "Proj/Menu.h" // Include the definition of the Menu class.
+#include "dataStructures/Graph.h"
+#include "dataParser.h"
+#include "functions.h"
+#include "menu.h"
+#include <set>
 
-int main(){
-    WaterSupply ws; // Create an instance of WaterSupply to manage the water supply network.
-    Menu menu; // Create an instance of Menu to handle user interactions.
 
-    menu.chooseDataSet(&ws); // Prompt the user to choose a data set for the water supply network.
+int main() {
+    //Graph<int> graph;
+    //ToyGraphParser("shipping.csv", graph);//error not fully connected
+    //ToyGraphParser("tourism.csv", graph);
+    //ToyGraphParser("stadiums.csv", graph);
+   //RealWorldGraphParser("graph2", graph);
+   //solveTSPBacktracking(graph, 0);
 
-    ws = WaterSupply(); // Re-initialize ws to reset or start with a new WaterSupply instance.
-    ws.buildNetwork(); // Build the network graph based on the data set chosen or default data.
+    // Depois para apresentar damos logo load aos 3 grafos
+    //Graph<int> toyGraph;
+    //Graph<int> extraGraph;
+    //Graph<int> realGraph;
 
-    ws.get_MaxFlow(cityF, ws); // Calculate the maximum flow from sources to sinks in the network.
+    Menu menu;
+    menu.start();
 
-    menu.mainMenu(ws); // Display the main menu of the application for further user interactions.
+    //ExtraFullyConnectedGraphsParser("edges_500.csv", graph, 500);
+    //RealWorldGraphParser("graph1", graph); // 1.14311e+06 - 5 min :(
+    //normalizeGraph(&graph);
 
-    return 0; // End the program.
+    //solveTSPBacktracking(graph, 0);
+    //solveTSPTriangularInequality(graph, 0);
+    return 0;
 }
